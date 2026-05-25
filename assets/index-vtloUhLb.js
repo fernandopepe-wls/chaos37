44cef457ed543868637719f39e9b06df195e129a
fexpepe <fernando.pepe@wildlifestudios.com>
2026-05-25 01:11:05 -0300

    deploy: bunny chaos 37 — github pages build

---
+++ assets/index-vtloUhLb.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-CLe66G07.js","assets/index-DEg4n1bS.js"])))=>i.map(i=>d[i]);
+import{_ as e}from"./index-BiUMqrTc.js";import{r as p}from"./index-DEg4n1bS.js";const _=p("App",{web:()=>e(()=>import("./web-CLe66G07.js"),__vite__mapDeps([0,1])).then(r=>new r.AppWeb)});export{_ as App};
